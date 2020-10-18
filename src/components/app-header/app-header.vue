12cc2be382ee37240c9d565d61bc2b2c1339c7a7
Shekhar Joshi <anna93@users.noreply.github.com>
2020-10-19 02:33:31 +0530

    Make mobile header pixel perfect

--- src/components/app-header/app-header.vue
+++ src/components/app-header/app-header.vue
@@ -1,12 +1,16 @@
 <template>
   <header
-    class="font-display bg-gray-900 relative flex justify-between items-center h-17 lg:h-20 text-white px-2 lg:pl-10 lg:pr-5 font-bold uppercase"
+    class="font-display bg-gray-900 relative flex justify-between items-center h-17 lg:h-20 text-white pr-3 pl-1 lg:pl-10 lg:pr-5 lg:font-bold uppercase"
   >
     <app-logo class="hidden lg:block" />
     <mobile-nav class="lg:hidden" />
 
     <desktop-nav-middle class="hidden lg:block" />
-    <app-logo class="lg:hidden block" />
+    <div
+      class="lg:hidden block absolute w-full flex left-0 right-0 justify-center"
+    >
+      <app-logo />
+    </div>
 
     <desktop-nav-icon-nav class="hidden lg:block" />
     <cart-icon class="lg:hidden block" />
--- src/components/app-header/desktop-nav/desktop-nav-middle.vue
+++ src/components/app-header/desktop-nav/desktop-nav-middle.vue
@@ -4,8 +4,14 @@
       <li v-for="navObj in centerNav" :key="navObj.title" class="mx-7">
         <a v-if="!navObj.showInner" href="">{{ navObj.title }}</a>
         <div v-else>
-          <span @click="selectInnerNav($event, navObj.title)">{{ navObj.title }}</span>
-          <component :class="openedInnerNav === navObj.title ? '' : 'hidden'" :is="navObj.component" v-bind="navObj.componentProps" />
+          <span @click="selectInnerNav($event, navObj.title)">{{
+            navObj.title
+          }}</span>
+          <component
+            :class="openedInnerNav === navObj.title ? '' : 'hidden'"
+            :is="navObj.component"
+            v-bind="navObj.componentProps"
+          />
         </div>
       </li>
     </ul>
@@ -24,7 +30,7 @@ export default {
   data() {
     return {
       centerNav: DESKTOP_NAV_CONFIG.centerNav,
-      openedInnerNav: '',
+      openedInnerNav: ""
     };
   },
   methods: {
