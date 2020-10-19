d67ac1bdc77cccf769fd544cf44ca9d96fcee866
Shekhar Joshi <anna93@users.noreply.github.com>
2020-10-19 14:39:50 +0530

    Write mobile search component

--- src/components/app-header/mobile-nav/mobile-nav-search.vue
+++ src/components/app-header/mobile-nav/mobile-nav-search.vue
@@ -1,5 +1,21 @@
 <template>
-  <div class="flex justify-between p-3 border-t border-gray-600">
-    <input />
+  <div class="flex justify-between p-3 border-t border-gray-600 font-light text-black relative">
+    <input
+      class="w-full h-14 rounded-full px-4"
+      placeholder="Search"
+      autofocus
+    />
+    <search-icon class="absolute text-gray-400 right-0 mr-8 top-half transform -translate-y-1/2" />
   </div>
 </template>
+
+<script>
+import SearchIcon from "../../icons/search-icon";
+
+export default {
+  name: "MobileNavSearch",
+  components: {
+    SearchIcon
+  }
+};
+</script>
